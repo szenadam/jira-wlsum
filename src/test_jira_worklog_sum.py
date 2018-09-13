3e6924a8ec46af3c59f3009796e7d0d2128ed4c8
szenadam <szenadam1@gmail.com>
2018-09-13 13:04:15 +0200

    Add tests
    Also set test verbosity to 2.

--- src/test_jira_worklog_sum.py
+++ src/test_jira_worklog_sum.py
@@ -4,14 +4,22 @@ from jira_worklog_sum import convert_to_seconds
 from jira_worklog_sum import check_arguments
 
 class TestConvertToSeconds(unittest.TestCase):
-    def test_hours(self):
-        self.assertEqual(convert_to_seconds('1h'), 3600)
-    def test_zero_hour(self):
-        self.assertEqual(convert_to_seconds('0h'), 0)
     def test_minutes(self):
         self.assertEqual(convert_to_seconds('35m'), 2100)
     def test_zero_minutes(self):
         self.assertEqual(convert_to_seconds('0m'), 0)
+    def test_hours(self):
+        self.assertEqual(convert_to_seconds('1h'), 3600)
+    def test_zero_hour(self):
+        self.assertEqual(convert_to_seconds('0h'), 0)
+    def test_days(self):
+        self.assertEqual(convert_to_seconds('1d'), 86400)
+    def test_zero_days(self):
+        self.assertEqual(convert_to_seconds('0d'), 0)
+    def test_weeks(self):
+        self.assertEqual(convert_to_seconds('1w'), 604800)
+    def test_zero_weeks(self):
+        self.assertEqual(convert_to_seconds('0w'), 0)
 
 class TestCheckArguments(unittest.TestCase):
     def test_zero_arguments(self):
@@ -20,4 +28,4 @@ class TestCheckArguments(unittest.TestCase):
         self.assertEqual(cm.exception.code, 1)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
