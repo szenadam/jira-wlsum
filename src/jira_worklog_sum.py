0ffc61f3571efc3018d15606bd2f1abc8963e47c
szenadam <szenadam1@gmail.com>
2018-09-14 10:53:43 +0200

    Major tabula rasa of the spreadsheet feature
    Start from scratch. Remove all worksheet writes. Go on step-by-step
    from extracting data and summing up worklogs for a day. Finally found
    a solution for grouping by multiple values and summing up a third value
    in a dictionary in Python. Extract almost everything into functions,
    which should make it easier later when the work will be done by classes
    and objects.
    Also create a matrix which will hold the actual results for the
    spreadsheet.
    Also rename check_arguments() to check_arguments_number() and
    use it in __main__.

--- src/jira_worklog_sum.py
+++ src/jira_worklog_sum.py
@@ -14,11 +14,13 @@ import sys
 import xlsxwriter
 import calendar
 from dateutil.tz import tzlocal
+from itertools import groupby
+from operator import itemgetter
 
-def check_arguments():
+def check_arguments_number():
   """ TODO: Use it somewhere """
   if len(sys.argv) < 3:
-    print('Usage: python jira_worklog_sum.py https://example.jira.com username password')
+    print('Not enough arguments!\nUsage: python jira_worklog_sum.py https://example.jira.com username password')
     exit(1)
 
 def convert_to_seconds(s):
@@ -52,50 +54,77 @@ def is_in_this_month(time_created):
   else:
     return False
 
+def get_logged_issues():
+  logged_issues = jira.search_issues('worklogAuthor = currentUser() AND worklogDate >= startOfMonth() ORDER BY key ASC')
+  return logged_issues
+
 def is_author_the_same(name):
+  """ Check if author is the same as currentUser() """
   return name == user_name
 
+def get_all_worklogs_for_issues(issues):
+  worklogs = []
+  for issue in issues:
+    for worklog in jira.worklogs(issue.key):
+      worklogs.append(worklog)
+  return worklogs
+
+def extract_data_from_worklogs(worklogs):
+  data = []
+  for worklog in worklogs:
+    d = {}
+    d['issueId'] = worklog.issueId
+    # The results should be in the local timezone. JIRA stores it in UTC0
+    d['dayStarted'] = dateutil.parser.parse(worklog.started).astimezone(tzlocal()).day
+    d['timeSpentSeconds'] = worklog.timeSpentSeconds
+    data.append(d)
+  return data
+
+def get_last_worklog_day(data):
+  last_day = 0
+  for d in data:
+    if d['dayStarted'] > last_day:
+      last_day = d['dayStarted']
+  return last_day
+
+def sum_up_worklog_for_a_day(data):
+  grouper = itemgetter("issueId", "dayStarted")
+  result = []
+  for key, grp in groupby(sorted(data, key = grouper), grouper):
+    temp_dict = dict(zip(["issueId", "dayStarted"], key))
+    temp_dict["timeSpentSeconds"] = sum(item["timeSpentSeconds"] for item in grp)
+    result.append(temp_dict)
+  return result
+
+def get_worklogs_total_seconds(data):
+  total = 0
+  for d in data:
+    total += d['timeSpentSeconds']
+  return total
 def main():
   """ The main loop. Loop through all logged issues in this month by the currentUser
   then loop through all the worklogs in that issue. """
 
-  logged_issues = jira.search_issues('worklogAuthor = currentUser() AND worklogDate >= startOfMonth() ORDER BY key ASC')
-  total_time_in_seconds = 0
+  logged_issues = get_logged_issues()
 
-  workbook = xlsxwriter.Workbook('gomontir_jira.xlsx')
-  worksheet = workbook.add_worksheet()
-  worksheet.write('A1', server_name + ' worklog')
-  worksheet.write('A2', user_name)
-  worksheet.write('A3', 'Key')
-  worksheet.write('B3', 'Summary')
+  worklogs = get_all_worklogs_for_issues(logged_issues)
 
-  current_year = date.today().year
-  current_month = date.today().month
-  current_month_short_name = calendar.month_name[current_month][:3]
-  start_of_this_month = date(current_year, current_month, 1)
-  today = date.today()
-  delta = today - start_of_this_month
+  extracted_data = extract_data_from_worklogs(worklogs)
 
-  for i in range(1, delta.days + 2):
-    worksheet.write(2, 1+i, current_month_short_name + ' ' + str(i).zfill(2))
+  total_time_in_seconds = get_worklogs_total_seconds(extracted_data)
 
-  for j, issue in enumerate(logged_issues):
+  number_of_issues = len(logged_issues)
+  last_day = get_last_worklog_day(extracted_data)
+  w, h = last_day, number_of_issues
+  calendar_matrix = [[0 for x in range(w)] for y in range(h)]
 
-    for worklog in jira.worklogs(issue.key):
-      if is_in_this_month(worklog.created) and is_author_the_same(worklog.author.name):
-        worksheet.write(3+j, 0, issue.key)
-        worksheet.write(3+j, 1, issue.fields.summary)
+  summed_up_data = sum_up_worklog_for_a_day(extracted_data)
 
-        day_created = dateutil.parser.parse(worklog.created).astimezone(tzlocal()).day
-        worksheet.write(3+j, day_created + 1, read_time(worklog.timeSpent) / 3600)
-
-        total_time_in_seconds += read_time(worklog.timeSpent)
-
-  workbook.close()
   print('Total hours spent:', total_time_in_seconds / 3600 )
 
 
 if __name__ == '__main__':
+  check_arguments_number()
   server_name=sys.argv[1]
   user_name=sys.argv[2]
   password=sys.argv[3]
