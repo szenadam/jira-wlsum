706779cd7bb661c9ae23b071bc4b57f01d507681
szenadam <szenadam1@gmail.com>
2018-09-17 11:13:08 +0200

    Create JiraExtractor class

--- src/jira_worklog_sum.py
+++ src/jira_worklog_sum.py
@@ -6,90 +6,15 @@
   Example output:
     $ Total hours spent: 10.2
 """
-from jira import JIRA
-from datetime import datetime, date
-import dateutil.parser
-import pytz
+from jiraextractor import JiraExtractor
+from datetime import date
 import sys
 import xlsxwriter
 import calendar
-from dateutil.tz import tzlocal
 from itertools import groupby
 from operator import itemgetter
 from natsort import natsorted
 import getopt
-
-
-def query_logged_issues():
-  """ Query the issues where worklogs were made by the currently logged in user
-    TODO: Later should handle dates if teh user is interested in a different time range.
-  """
-  logged_issues = jira.search_issues('worklogAuthor = currentUser() AND worklogDate >= startOfMonth() ORDER BY key ASC')
-  return logged_issues
-
-
-def is_in_this_month(time_created):
-  """ Need to check each worklog, otherwise it would sum every time spent for the
-  current user. Hopefully the worklog.created is in UTC0. """
-
-  created = dateutil.parser.parse(time_created)
-  today = datetime.today()
-  if created > datetime(today.year,today.month,1,0,0,0,0,pytz.UTC):
-    return True
-  else:
-    return False
-
-
-def get_all_worklogs_for_issues(issues):
-  """ Extract all worklog objects into a list. Add issue key to the worklog object. """
-  worklogs = []
-  for issue in issues:
-    for worklog in jira.worklogs(issue.key):
-      if is_in_this_month(worklog.started):
-        worklog.key = issue.key
-        worklogs.append(worklog)
-  return worklogs
-
-
-def extract_data_from_worklogs(worklogs):
-  """ Extract only the necessary attribues from the worklog objects. """
-  data = []
-  for worklog in worklogs:
-    d = {}
-    d['issueKey'] = worklog.key
-    # The results should be in the local timezone. JIRA stores it in UTC0
-    d['dayStarted'] = dateutil.parser.parse(worklog.started).astimezone(tzlocal()).day
-    d['timeSpentSeconds'] = worklog.timeSpentSeconds
-    data.append(d)
-  return data
-
-
-def get_last_worklog_day(data):
-  """ Last day that has a worklog. Used for the calendar matrix width. """
-  last_day = 0
-  for d in data:
-    if d['dayStarted'] > last_day:
-      last_day = d['dayStarted']
-  return last_day
-
-
-def sum_up_worklog_for_a_day(data):
-  """ Key function for aggregating the time spent in an issue a day. """
-  grouper = itemgetter("issueKey", "dayStarted")
-  result = []
-  for key, grp in groupby(sorted(data, key = grouper), grouper):
-    temp_dict = dict(zip(["issueKey", "dayStarted"], key))
-    temp_dict["timeSpentSeconds"] = sum(item["timeSpentSeconds"] for item in grp)
-    result.append(temp_dict)
-  return result
-
-
-def get_worklogs_total_seconds(data):
-  """ Sum up all the time spent. """
-  total = 0
-  for d in data:
-    total += d['timeSpentSeconds']
-  return total
 
 
 def get_uniq_keys(data):
@@ -100,6 +25,15 @@ def get_uniq_keys(data):
     uniq.append(d['issueKey'])
   return uniq
 
+def sum_up_worklog_for_a_day(data):
+  """ Key function for aggregating the time spent in an issue a day. """
+  grouper = itemgetter("issueKey", "dayStarted")
+  result = []
+  for key, grp in groupby(sorted(data, key = grouper), grouper):
+    temp_dict = dict(zip(["issueKey", "dayStarted"], key))
+    temp_dict["timeSpentSeconds"] = sum(item["timeSpentSeconds"] for item in grp)
+    result.append(temp_dict)
+  return result
 
 def create_calendar_data_matrix(rows, columns, data):
   """ Create the calendar data matrix from an extracted worklog list.
@@ -187,19 +121,21 @@ def generate_spreadsheet(data_matrix, workbook_name='worklog.xlsx', description_
   workbook.close()
 
 
-def main():
+def main(server_name, user_name, password):
   """ The main loop. """
 
-  logged_issues = query_logged_issues()
+  jira = JiraExtractor(server_name, user_name, password)
+
+  logged_issues = jira.query_logged_issues()
   calendar_description_matrix = create_description_matrix(logged_issues)
-  worklogs = get_all_worklogs_for_issues(logged_issues)
+  worklogs = jira.get_all_worklogs_for_issues(logged_issues)
 
-  extracted_data = extract_data_from_worklogs(worklogs)
+  extracted_data = jira.extract_data_from_worklogs(worklogs)
 
-  total_time_in_seconds = get_worklogs_total_seconds(extracted_data)
+  total_time_in_seconds = jira.get_worklogs_total_seconds(extracted_data)
 
   number_of_issues = len(logged_issues)
-  last_day = get_last_worklog_day(extracted_data)
+  last_day = jira.get_last_worklog_day(extracted_data)
 
   calendar_matrix_data = create_calendar_data_matrix(number_of_issues, last_day, extracted_data)
 
@@ -249,6 +185,4 @@ if __name__ == '__main__':
       output_name = arg
     else:
       assert False, "unhandled option"
-
-  jira = JIRA(server_name, basic_auth=(user_name, password))
-  main()
+  main(server_name, user_name, password)
---
+++ src/jiraextractor.py
@@ -0,0 +1,72 @@
+from jira import JIRA
+from datetime import datetime, date
+from dateutil.tz import tzlocal
+import dateutil.parser
+import pytz
+
+class JiraExtractor():
+  def __init__(self, server, username, password):
+    self.server = server
+    self.username = username
+    self.password = password
+    self.jira = JIRA(self.server, basic_auth=(self.username, self.password))
+
+
+  def query_logged_issues(self):
+    """ Query the issues where worklogs were made by the currently logged in user
+      TODO: Later should handle dates if teh user is interested in a different time range.
+    """
+    logged_issues = self.jira.search_issues('worklogAuthor = currentUser() AND worklogDate >= startOfMonth() ORDER BY key ASC')
+    return logged_issues
+
+
+  def is_in_this_month(self, time_created):
+    """ Need to check each worklog, otherwise it would sum every time spent for the
+    current user. Hopefully the worklog.created is in UTC0. """
+
+    created = dateutil.parser.parse(time_created)
+    today = datetime.today()
+    if created > datetime(today.year,today.month,1,0,0,0,0,pytz.UTC):
+      return True
+    else:
+      return False
+
+
+  def get_all_worklogs_for_issues(self, issues):
+    """ Extract all worklog objects into a list. Add issue key to the worklog object. """
+    worklogs = []
+    for issue in issues:
+      for worklog in self.jira.worklogs(issue.key):
+        if self.is_in_this_month(worklog.started):
+          worklog.key = issue.key
+          worklogs.append(worklog)
+    return worklogs
+
+
+  def extract_data_from_worklogs(self, worklogs):
+    """ Extract only the necessary attribues from the worklog objects. """
+    data = []
+    for worklog in worklogs:
+      d = {}
+      d['issueKey'] = worklog.key
+      # The results should be in the local timezone. JIRA stores it in UTC0
+      d['dayStarted'] = dateutil.parser.parse(worklog.started).astimezone(tzlocal()).day
+      d['timeSpentSeconds'] = worklog.timeSpentSeconds
+      data.append(d)
+    return data
+
+
+  def get_last_worklog_day(self, data):
+    """ Last day that has a worklog. Used for the calendar matrix width. """
+    last_day = 0
+    for d in data:
+      if d['dayStarted'] > last_day:
+        last_day = d['dayStarted']
+    return last_day
+
+  def get_worklogs_total_seconds(self, data):
+    """ Sum up all the time spent. """
+    total = 0
+    for d in data:
+      total += d['timeSpentSeconds']
+    return total
