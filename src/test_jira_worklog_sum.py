39d0d73db7829155399868697375f68d3a1c61c9
szenadam <szenadam1@gmail.com>
2018-09-23 11:50:27 +0200

    Clean test file

--- src/test_jira_worklog_sum.py
+++ src/test_jira_worklog_sum.py
@@ -1,12 +1,5 @@
 import unittest
 
-from jira_worklog_sum import check_arguments_number
-
-class TestCheckArguments(unittest.TestCase):
-    def test_zero_arguments(self):
-        with self.assertRaises(SystemExit) as cm:
-            check_arguments_number()
-        self.assertEqual(cm.exception.code, 1)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
